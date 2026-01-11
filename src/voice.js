36880354da3e925563871224f0957e129a82aba2
Fumikazu Fujiwara | Freddie <github@ze.gs>
2026-01-11 12:26:34 +0900

    Add voice text length guard

--- src/voice.js
+++ src/voice.js
@@ -3,6 +3,10 @@ import { sanitizeText } from "./text.js";
 
 const VOICE_HOSTS = { speak: DEVICE_HOSTS.nest, speakTatami: DEVICE_HOSTS.tatami };
 const CAR_ARRIVAL_MESSAGE = "チエミさん、ママさん、パパが到着しました。準備をお願いします。";
+export const MAX_VOICE_TEXT =
+  "１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３4";
+const MAX_ENCODED_LENGTH = encodeURIComponent(MAX_VOICE_TEXT).length;
+const isVoiceTextTooLong = (voiceText) => sanitizeText(voiceText).length > MAX_ENCODED_LENGTH;
 
 export const buildVoiceUrls = (voiceText) => {
   const sanitized = sanitizeText(voiceText);
@@ -13,11 +17,16 @@ export const buildVoiceUrls = (voiceText) => {
 };
 
 export const updateVoiceLinks = (voiceText, elements) => {
+  if (isVoiceTextTooLong(voiceText)) {
+    console.error(`Too long text : ${voiceText}`);
+    return false;
+  }
   const { speak, speakTatami } = buildVoiceUrls(voiceText);
   elements.speak.dataset.url = speak;
   elements.speakTatami.dataset.url = speakTatami;
   elements.speak.setAttribute("href", "#");
   elements.speakTatami.setAttribute("href", "#");
+  return true;
 };
 
 export const buildCarArrivalArgs = () => [
